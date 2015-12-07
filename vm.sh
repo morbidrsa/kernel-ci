802781792ccdd00afc10c8a4c953928c39c226b1
Johannes Thumshirn <jthumshirn@suse.de>
2015-12-07 16:03:46 +0100

    vm.sh: Don't use USB
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>

--- vm.sh
+++ vm.sh
@@ -41,7 +41,7 @@ done
 
 QEMU_CMD="$QEMU$ARCH"
 
-CLI_DEFAULTS="-device piix3-usb-uhci,id=usb,bus=pci.0,addr=0x1.0x2 -device virtio-serial-pci,id=virtio-serial0,bus=pci.0,addr=0x4 -device virtio-balloon-pci,id=balloon0,bus=pci.0,addr=0x6"
+CLI_DEFAULTS="-device virtio-serial-pci,id=virtio-serial0,bus=pci.0,addr=0x4 -device virtio-balloon-pci,id=balloon0,bus=pci.0,addr=0x6"
 CLI_HDD="-device virtio-blk-pci,scsi=off,bus=pci.0,addr=0x5,drive=drive-virtio-disk0,id=virtio-disk0,bootindex=1 -drive file=${ROOTFS},if=none,id=drive-virtio-disk0,format=qcow2 -snapshot"
 CLI_SERIAL="-serial stdio -monitor none"
 CLI_BOOT=""
